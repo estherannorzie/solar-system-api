027ca21e7bf6fcd1bfbe279b1c3be5e50a056aa1
Esther Annorzie <estherannorzie@gmail.com>
2022-04-22 14:04:45 -0400

    finished wave 1

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planets:
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
+
+
+# create our instances
+planets = [
+    Planets(1, "Mercury", "Small hot planet"),
+    Planets(2, "Venus", "A gaseous planet"),
+    Planets(3, "Earth", "Has lots of life")
+]
+
+# create the blueprint
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+# do the decorator
+@planets_bp.route("", methods=["GET"])
+def get_all_planets():
+    all_planets = []
+    for planet in planets:
+        all_planets.append(
+            {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description
+            }
+        )
+    return jsonify(all_planets)
