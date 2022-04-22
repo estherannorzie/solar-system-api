1de456e894ed238cf93405b0355835a308a14d0f
Esther Annorzie <estherannorzie@gmail.com>
2022-04-22 19:59:49 -0400

    cleaned up get_all_planets again

--- app/routes.py
+++ app/routes.py
@@ -20,12 +20,9 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 # do the decorator
 @planets_bp.route("", methods=["GET"])
 def get_all_planets():
-    all_planets = [{
-                    "id": planet.id,
-                    "name": planet.name,
-                    "description": planet.description
-                    } 
-                    for planet in planets]
-
-
-    return jsonify(all_planets)
+    return jsonify([
+        {"id": planet.id,
+        "name": planet.name,
+        "description": planet.description} 
+        for planet in planets
+    ])
