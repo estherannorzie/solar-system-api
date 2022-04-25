478f69b8fc4edba4cf1855034a522034655969b3
Danielle <dwhyte1226@gmail.com>
2022-04-25 13:55:47 -0500

    created new endpoint to return one planet by planet id

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planets:
     def __init__(self, id, name, description):
@@ -21,8 +21,29 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 @planets_bp.route("", methods=["GET"])
 def get_all_planets():
     return jsonify([
-        {"id": planet.id,
-        "name": planet.name,
-        "description": planet.description} 
+         to_dict(planet)
         for planet in planets
-    ])
+  ])
+
+@planets_bp.route("/<planet_id>", methods = ["GET"])
+def get_one_planet(planet_id):
+
+    planet = validate_planet(planet_id)
+
+    return jsonify(to_dict(planet))
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        abort(make_response({"message": f"invalid id {planet_id}"}, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    abort(make_response({"message": f"planet id {planet_id} not found"}, 404))
+
+def to_dict(planet):
+    return {"id": planet.id,
+        "name": planet.name,
+        "description": planet.description}
