28c5ea7c19b36f08d9b21451b1b05e51f54292bc
Esther Annorzie <estherannorzie@gmail.com>
2022-04-29 14:25:44 -0400

    started creating database and planet model

--- app/routes.py
+++ app/routes.py
@@ -1,49 +1,47 @@
 from flask import Blueprint, jsonify, abort, make_response
+from app.models.planet import Planet
+from app import db
 
-class Planets:
-    def __init__(self, id, name, description):
-        self.id = id
-        self.name = name
-        self.description = description
+# class Planets:
+#     def __init__(self, id, name, description):
+#         self.id = id
+#         self.name = name
+#         self.description = description
 
 
 # create our instances
-planets = [
-    Planets(1, "Mercury", "Small hot planet"),
-    Planets(2, "Venus", "A gaseous planet"),
-    Planets(3, "Earth", "Has lots of life")
-]
+# planets = [
+#     Planets(1, "Mercury", "Small hot planet"),
+#     Planets(2, "Venus", "A gaseous planet"),
+#     Planets(3, "Earth", "Has lots of life")
+# ]
 
 # create the blueprint
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
+
 # do the decorator
-@planets_bp.route("", methods=["GET"])
-def get_all_planets():
-    return jsonify([
-         to_dict(planet)
-        for planet in planets
-  ])
+# @planets_bp.route("", methods=["GET"])
+# def get_all_planets():
+#     return jsonify([
+#          to_dict(planet)
+#         for planet in planets
+#   ])
 
-@planets_bp.route("/<planet_id>", methods = ["GET"])
-def get_one_planet(planet_id):
+# @planets_bp.route("/<planet_id>", methods = ["GET"])
+# def get_one_planet(planet_id):
 
-    planet = validate_planet(planet_id)
+#     planet = validate_planet(planet_id)
 
-    return jsonify(to_dict(planet))
+#     return jsonify(to_dict(planet))
 
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except ValueError:
-        abort(make_response({"message": f"invalid id {planet_id}"}, 400))
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except ValueError:
+#         abort(make_response({"message": f"invalid id {planet_id}"}, 400))
 
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
-    abort(make_response({"message": f"planet id {planet_id} not found"}, 404))
-
-def to_dict(planet):
-    return {"id": planet.id,
-        "name": planet.name,
-        "description": planet.description}
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
+#     abort(make_response({"message": f"planet id {planet_id} not found"}, 404))
