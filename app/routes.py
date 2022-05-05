07aa8502f834456751bd2d7cfc793bba8b499d2d
Danielle <dwhyte1226@gmail.com>
2022-05-05 14:18:17 -0500

    refactored code to add jsonify_message

--- app/routes.py
+++ app/routes.py
@@ -13,34 +13,23 @@ def create_planet():
     db.session.add(new_planet)
     db.session.commit()
 
-    return make_response(jsonify(f"Planet {new_planet.name} successfully created"), 201)
+    return jsonify_message(f"Planet {new_planet.name} successfully created", 201)
 
 @planets_bp.route("", methods=["GET"])
 def read_all_planets():
-    
     name_query = request.args.get("name")
     if name_query:
         planets = Planet.query.filter_by(name=name_query)
     else: 
         planets = Planet.query.all() 
-    # description_query = request.args.get("description")
-    # if description_query:
-    #     planets.extend(Planet.query.filter_by(description=description_query))
-    
-    # if not planets:
-    #     planets = Planet.query.all()  
 
-    planets_response = []
-    for planet in planets:
-        planets_response.append(planet.to_dict())
+    planets_response = [planet.to_dict() for planet in planets]
     return jsonify(planets_response)
 
 
 @planets_bp.route("/<planet_id>", methods = ["GET"])
 def get_one_planet(planet_id):
-
     planet = validate_planet(planet_id)
-
     return jsonify(planet.to_dict())
 
 
@@ -55,7 +44,7 @@ def update_planet(planet_id):
 
     db.session.commit()
     
-    return make_response(jsonify(f"Planet succesfully updated. {planet.to_dict()}"))
+    return jsonify_message(f"Planet succesfully updated. {planet.to_dict()}")
 
 
 @planets_bp.route("/<planet_id>", methods = ["DELETE"])
@@ -65,7 +54,7 @@ def delete_planet(planet_id):
     db.session.delete(planet)
     db.session.commit()
     
-    return make_response(jsonify(f"Planet succesffuly deleted."))
+    return jsonify_message("Planet succesfully deleted.", 200)
 
 def validate_planet(planet_id):
     try:
@@ -78,3 +67,7 @@ def validate_planet(planet_id):
     if not planet:
         abort(make_response({"message": f"planet id {planet_id} not found"}, 404))
     return planet
+
+
+def jsonify_message(message, status_code):
+    return make_response(jsonify(message),status_code)
